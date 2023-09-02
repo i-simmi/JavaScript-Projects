fe1c2ae87bf258fec7492946665abb23d4216302
Simmi Sharma <simmi@Simmis-MacBook-Air.local>
2023-09-02 18:05:05 +0530

    .

--- 17_Movie_App/script.js
+++ 17_Movie_App/script.js
@@ -1,7 +1,7 @@
 const main = document.getElementById('main')
 const API_URL ='https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=8a2a9a2fa148eb24b64ff2cfa97f545c&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=8a2a9a2fa148eb24b64ff2cfa97f545c&query="'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=8a2a9a2fa148eb24b64ff2cfa97f545c&query= '
 
 const form = document.getElementById('form')
 const search = document.getElementById('search')
@@ -20,22 +20,24 @@ function showMovies(movies){
     main.innerHTML = ''
 
     movies.forEach((movie) => {
-        const { title, poster_path,vote_average, overview} = movie
-    
-        const movieEl = document.createElement('div')
-        movieEl.classList.add('movie')
-        movieEl.innerHTML = `
-            <img src="${IMG_PATH + poster_path}" alt="${title}">
-            <div class="movie-info">
-                <h3>${title}</h3>
-                <span class="${getClassByRate(vote_average)}">${vote_average}</span>    
-            </div>
-            <div class="overview">
-                <h3>Overview</h3>
-                ${overview}
-            </div>
-        `
-        main.appendChild(movieEl)
+        const {title, poster_path,vote_average, overview} = movie
+        if(title && poster_path && vote_average && overview){
+            const movieEl = document.createElement('div')
+            movieEl.classList.add('movie')  // Way to add one or more classes to an element
+            movieEl.innerHTML = `
+                <img src="${IMG_PATH + poster_path}" alt="${title}">
+                <div class="movie-info">
+                    <h3>${title}</h3>
+                    <span class="${getClassByRate(vote_average)}">${vote_average}</span>    
+                </div>
+                <div class="overview">
+                    <h3>Overview</h3>
+                    ${overview}
+                </div>
+            `
+            main.appendChild(movieEl)
+        }
+
     });
 
 }
